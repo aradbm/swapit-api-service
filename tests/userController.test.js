ee368df62d6de2e8781053ae56d0c21de96e4d8f
Arad Ben Menashe <aradbm@gmail.com>
2024-06-08 02:35:42 +0300

    Add Jest for unit testing

---
+++ tests/userController.test.js
@@ -0,0 +1,50 @@
+// tests/userController.test.js
+const UserModel = require("../models/user");
+const userController = require("../controllers/userController");
+const { message } = require("statuses");
+
+// Mock the UserModel methods
+jest.mock("../models/user");
+
+describe("User Controller", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should get an existing user", async () => {
+    const mockUser = { uid: "123", lastlogin: new Date() };
+    UserModel.getUserById.mockResolvedValue(mockUser);
+    UserModel.updateUser.mockResolvedValue(mockUser);
+
+    const req = { params: { id: "123" } };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+
+    await userController.getUser(req, res);
+
+    expect(UserModel.getUserById).toHaveBeenCalledWith("123");
+    expect(UserModel.updateUser).toHaveBeenCalledWith("123");
+    expect(res.json).toHaveBeenCalledWith({
+      user: mockUser,
+      token: expect.any(String),
+    });
+  });
+
+  it("should return an error if the user does not exist", async () => {
+    UserModel.getUserById.mockResolvedValue(null);
+
+    const req = { params: { id: "123" } };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+
+    await userController.getUser(req, res);
+
+    expect(UserModel.getUserById).toHaveBeenCalledWith("123");
+    expect(res.status).toHaveBeenCalledWith(400); // Bad Request
+    expect(res.json).toHaveBeenCalledWith({ message: "Unable to create user" });
+  });
+});
