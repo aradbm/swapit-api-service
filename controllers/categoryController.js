3479b163a03be9b44227bbf49f3cfefe13636bea
Arad Ben Menashe <aradbm@gmail.com>
2024-06-09 23:02:46 +0300

    added Redis caching for category retrieval

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,11 +1,25 @@
 const UserModel = require("../models/category");
-
+const { redisClient } = require("../config/redisDB");
 const getAllCategories = async (req, res) => {
-  console.log("Fetching all categories");
+  console.log("Fetching all categories...");
   try {
+    const categoriesCache = await redisClient.get("categories");
+    if (categoriesCache) {
+      console.log("Categories fetched from cache");
+      return res.json(JSON.parse(categoriesCache));
+    }
+
     const categories = await UserModel.getCategories();
+    if (!categories) {
+      return res.status(500).json({ message: "Server error" });
+    }
+
+    // set cache for 10 minutes
+    redisClient.setEx("categories", 600, JSON.stringify(categories));
+    console.log("Categories fetched successfully");
     res.json(categories);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Server error" });
   }
 };
